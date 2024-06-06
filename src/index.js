3e038062da927eb6820104670b5674984ba9c5dd
Farshad <f.bagdeli13@gmail.com>
2024-06-06 12:55:50 +0200

    prudct Class Done

---
+++ src/index.js
@@ -0,0 +1,17 @@
+class Product {
+  #pName
+  #pPrice
+  #pDescription
+  constructor(pName, pPrice, pDescription){
+    this.pName = pName,
+    this.pPrice = pPrice,
+    this.#pDescription = pDescription
+  }
+
+  details(){
+    return `
+    Product Name is : ${this.pName}, Price is : ${this.pPrice} .
+    ${this.#pDescription}.
+    `
+  }
+}
