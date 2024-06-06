b9894bf70db84839935e50da5be7ac3ac6f4d56f
Farshad <f.bagdeli13@gmail.com>
2024-06-06 12:58:32 +0200

    prudct Class Private Done

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ class Product {
   #pPrice
   #pDescription
   constructor(pName, pPrice, pDescription){
-    this.pName = pName,
-    this.pPrice = pPrice,
+    this.#pName = pName,
+    this.#pPrice = pPrice,
     this.#pDescription = pDescription
   }
 
   details(){
     return `
-    Product Name is : ${this.pName}, Price is : ${this.pPrice} .
+    Product Name is : ${this.#pName}, Price is : ${this.#pPrice} .
     ${this.#pDescription}.
     `
   }
