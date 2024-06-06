7142fa147373d3cde30ab8a40b79bb3b60f10b0d
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-06 12:28:19 +0200

    create tests to see if products exist

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,13 +1,19 @@
-import Basket from "../src/index.js"
+import Basket, { Product } from "../src/index.js"
 
 describe('Basket', () => {
     let basket
+    let eggs
+    let yoghurt
 
     beforeEach(() => {
         basket = new Basket
+        eggs = new Product('eggs', 2.30, 'grassfed freerange chicken eggs')
+        yoghurt = new Product('yoghurt', 1.45, 'grassfed freerange cow milk')
     })
 
     it('should exist', () => {
         expect(basket).toBeInstanceOf(Basket)
+        expect(eggs).toBeInstanceOf(Product)
+        expect(yoghurt).toBeInstanceOf(Product)
     })
 })
--- src/index.js
+++ src/index.js
@@ -13,3 +13,4 @@ class Product {
 }
 
 export default Basket
+export { Product }
