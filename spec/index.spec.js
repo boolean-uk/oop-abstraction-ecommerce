595df5624a3c2ea2521b452e4166ae21a5f2f6d1
Tim Zoltie <t.zoltie@gmail.com>
2024-06-06 17:45:14 +0100

    find product in product list, add product to basket test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -4,13 +4,20 @@ import Product from "../src/product.js"
 
 describe('Basket', () => {
     let basket
+    let productList
     beforeEach(() => {
-        basket = new Basket
+        basket = new Basket()
+        productList = new ProductList()
     })
     it('should find the product from the product list', () => {
-        const productList = new ProductList()
         const watch = productList.getProduct('Garmin')
         expect(productList).toBeInstanceOf(ProductList)
         expect(watch.name).toBe('Garmin epix Gen 2')
+    })
+    it('should add a product to the basket', () => {
+        const watch = productList.getProduct('Garmin')
+        basket.add(watch)
+        expect(basket.basket.length).toBe(1)
+        expect(basket.basket[0].name).toBe('Garmin epix Gen 2')
     })
 })
