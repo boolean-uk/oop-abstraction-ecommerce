330ad78a60daa5fe23fac662cbcd30c2ffc86173
Shaun Harris <shaun@shaunwilliamharris.com>
2024-06-06 11:46:06 +0100

    initial product set up, domain model and test 1 for product to ensure product matched expected-Passed

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -5,7 +5,7 @@ describe('Product', () => {
         const product = new Product(
             'Van Gogh Print',
             100,
-            'The Starry Night by Van Dyke, Canvas Print, 140cm(w)x 100cm(h)'
+            'The Starry Night by Van Gogh, Canvas Print, 140cm(w)x 100cm(h)'
         )
         expect(product.getProductDetails()).toBe(
             'Van Gogh Print: $100 - The Starry Night by Van Gogh, Canvas Print, 140cm(w)x 100cm(h)'
